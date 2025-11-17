aacba603bcc5b9c6a27e2d21cbdd962b51094284
allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-11-17 15:30:18 +0200

    Small permissions fix

--- main.js
+++ main.js
@@ -306,7 +306,7 @@
         deleteMessage.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>`;
         deleteMessage.classList.add("messageControlMenuButton");
         deleteMessage.classList.add("messageDeleteButton");
-        deleteMessage.style.display = checkPermissions("delete") ? "block" : "none";
+        if (msg.user !== userData.username) deleteMessage.style.display = checkPermissions("delete") ? "block" : "none";
         deleteMessage.onclick = () => ws.send(`{"cmd":"message_delete", "channel":"${currentChannel}", "id":"${msg.id}"}`);
         const reactMessage = document.createElement("button");
         reactMessage.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-smile-plus-icon lucide-smile-plus"><path d="M22 11v1a10 10 0 1 1-9-10"/><path d="M8 14s1.5 2 4 2 4-2 4-2"/><line x1="9" x2="9.01" y1="9" y2="9"/><line x1="15" x2="15.01" y1="9" y2="9"/><path d="M16 5h6"/><path d="M19 2v6"/></svg>`;
