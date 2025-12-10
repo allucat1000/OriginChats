bdc6c28719ed0a38464708e518691251f2d7007c
allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-12-11 00:23:40 +0200

    Better pin button

--- main.js
+++ main.js
@@ -1219,8 +1219,9 @@ async function initUI() {
     const pinButton = document.querySelector("#viewPins");
     pinButton.onclick = async() => {
         if (!pinsMenuOpen) {
-            pinsMenuOpen = true;
             if (!currentChannel) return;
+            pinButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="white" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pin-icon lucide-pin"><path d="M12 17v5"/><path d="M9 10.76a2 2 0 0 1-1.11 1.79l-1.78.9A2 2 0 0 0 5 15.24V16a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-.76a2 2 0 0 0-1.11-1.79l-1.78-.9A2 2 0 0 1 15 10.76V7a1 1 0 0 1 1-1 2 2 0 0 0 0-4H8a2 2 0 0 0 0 4 1 1 0 0 1 1 1z"/></svg>`;
+            pinsMenuOpen = true;
             pinsDiv.innerHTML = `<h2 class="pinsTitle">Pinned messages</h2>`;
             const loading = document.createElement("p");
             loading.classList.add("pinsTitle");
@@ -1240,6 +1241,7 @@ async function initUI() {
             loading.remove();
             pinsDiv.append(f);
         } else {
+            pinButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pin-icon lucide-pin"><path d="M12 17v5"/><path d="M9 10.76a2 2 0 0 1-1.11 1.79l-1.78.9A2 2 0 0 0 5 15.24V16a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-.76a2 2 0 0 0-1.11-1.79l-1.78-.9A2 2 0 0 1 15 10.76V7a1 1 0 0 1 1-1 2 2 0 0 0 0-4H8a2 2 0 0 0 0 4 1 1 0 0 1 1 1z"/></svg>`;
             pinsDiv.remove();
             pinsMenuOpen = false;
         }
