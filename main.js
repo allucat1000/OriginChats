182d5e99ef0c34012914f110fbd83f34883f0a1b
allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-12-10 22:59:51 +0200

    Permissions fix

--- main.js
+++ main.js
@@ -429,6 +429,7 @@ async function buildMessage(msg, group, old = false) {
     editMessage.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil-icon lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg>`;
     editMessage.classList.add("messageControlMenuButton");
     editMessage.classList.add("messageEditButton");
+    if (msg.user !== userData.username) editMessage.style.display = checkPermissions("edit") ? "block" : "none";
     editMessage.onclick = () => {
         messageHandling = { type: "edit", id: msg.id, curInputPlaceholder: chatInput.placeholder }
         if (!ratelimit.active) {
